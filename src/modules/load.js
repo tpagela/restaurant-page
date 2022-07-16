e8f47e7b505ec414edefd3f5fa14ac2476358b20
Tyler Pagela <tpagela@ucsd.edu>
2022-07-15 22:40:46 -0700

    Initial Page load finished

--- src/modules/load.js
+++ src/modules/load.js
@@ -1,26 +1,36 @@
 function loadPage() {
     const content = document.getElementById("content");
     const header = document.createElement("div");
-    header.setAttribute('id', 'header');
+    header.setAttribute('id', 'header',);
     const backgroundImage = document.createElement("div");
     backgroundImage.setAttribute('id', 'background');
     
     const item1 = document.createElement('ul');
     item1.innerHTML = ("Home")
+    item1.setAttribute('class', 'tablinks');
     
     const item2 = document.createElement('ul');
     item2.innerHTML = ("Menu")
+    item2.setAttribute('class', 'tablinks');
     
     const item3 = document.createElement('ul');
     item3.innerHTML = ("About Us")
+    item3.setAttribute('class', 'tablinks');
+
+    const mainText = document.createElement('div');
+    mainText.setAttribute('id', 'backgroundText')
+    mainText.innerHTML = ("HOT CHICK SWEET CHICK MAIN CHICK")
+    backgroundImage.appendChild(mainText);
     
     content.appendChild(header);
     content.appendChild(backgroundImage)
     header.appendChild(item1)
     header.appendChild(item2)
     header.appendChild(item3)
-    console.log(content)
+    console.log(item1);
 };
+
+
 
 export default loadPage;
 
